b956ee716175b9f51c6df2ccee85385d51d992c2
Sujan Tamang <tamang.sujan@worldlink.com.np>
2022-12-30 08:07:20 +0545

    UNKNOWN case handled

--- check_ha_services.sh
+++ check_ha_services.sh
@@ -2,7 +2,7 @@
 #26th Dec 2022
 #Sujan Tamang
 #To monitor service as a service basis
-
+#set -x
 
 GOOD=0
 WARNING=1
@@ -39,16 +39,23 @@ function check_individual_service {
 		usage
 	fi
 
+	get_output=$(snmpget -v 2c -c snmpAgent $hostname SNMPv2-MIB::sysDescr.0)
+
+	if [ -z "$get_output" ]
+	then
+		echo $UNKNOWN
+	fi
+
 	pid=`snmpwalk -v 2c -c snmpAgent $hostname HOST-RESOURCES-MIB::hrSWRunPath | grep -w "$process" | awk -F'=' '{print $1}' | awk -F'.' '{print $NF}' | head -1`
 
 	if [ -z "$pid" ]
 	then
-		echo 0
+		echo $CRITICAL
 	else
 		service_status=`snmpwalk -v 2c -c  snmpAgent $hostname HOST-RESOURCES-MIB::hrSWRunStatus.${pid} | awk -F'=' '{print $2}'`
 		if echo "$service_status" | grep -q runnable || echo "$service_status" | grep -q running;
 		then
-			echo 1
+			echo $GOOD
 		fi
 	fi
 }
@@ -68,26 +75,33 @@ function get_responses {
 
 
 function main {
+	no_of_hosts=$(echo "$# - 1" | bc)
 	responses=$(get_responses $@)
-	if ! echo ${responses} | grep -q 1;then
-		echo "$@ services down.."
+	if [ $(echo ${responses} | grep -o $CRITICAL | wc -w) -eq $no_of_hosts ]; then
+		echo "$@  down.."
 		exit $CRITICAL
-	elif ! echo ${responses} | grep -q 0;then
-		echo "$@ services running.."
+	elif [ $(echo ${responses} | grep -o $GOOD | wc -w) -eq $no_of_hosts ]; then
+		echo "$@  running.."
 		exit $GOOD
+	elif [ $(echo ${responses} | grep -o $UNKNOWN | wc -w) -eq $no_of_hosts ]; then
+		echo "$@  UNKNOWN.."
+		exit $UNKNOWN
 	else
 		j=2
 		host_with_service_down=""
 		host_with_service_up=""
+		host_with_service_unknown=""
 		for response in ${responses};do
-			if [ $response -eq 0 ];then
+			if [ $response -eq $CRITICAL ];then
 				host_with_service_down=$host_with_service_down" ${!j}"
-			else
+			elif [ $response -eq $GOOD ];then
 				host_with_service_up=$host_with_service_up" ${!j}"
+			else
+				host_with_service_unknown=$host_with_service_unknown" ${!j}"
 			fi
 			j=$(( j + 1 ))
 		done;
-		echo "$1 down: ${host_with_service_down} up:${host_with_service_up}"
+		echo "${!1} down: ${host_with_service_down} up:${host_with_service_up} unknown: ${host_with_service_unknown}"
 		exit $WARNING
 	fi;
 }
